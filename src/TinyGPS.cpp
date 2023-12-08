f9012dcc0ef32e84a3e74dcd2c87c5aae5545972
cwallukat <cwallukat@gmx.at>
2023-12-08 05:20:32 +0100

    Added GPxxx Message support... The cheap BN220 sends NMEA messages using the Term GPxxx instead of GNxxx

--- src/TinyGPS.cpp
+++ src/TinyGPS.cpp
@@ -25,6 +25,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #define _GPRMC_TERM   "GPRMC"
 #define _GPGGA_TERM   "GPGGA"
+#define _GNRMC_TERM   "GNRMC"
+#define _GNGGA_TERM   "GNGGA"
 
 TinyGPS::TinyGPS()
   :  _time(GPS_INVALID_TIME)
@@ -214,7 +216,11 @@ bool TinyGPS::term_complete()
   {
     if (!gpsstrcmp(_term, _GPRMC_TERM))
       _sentence_type = _GPS_SENTENCE_GPRMC;
+    else if (!gpsstrcmp(_term, _GNRMC_TERM))
+      _sentence_type = _GPS_SENTENCE_GPRMC;
     else if (!gpsstrcmp(_term, _GPGGA_TERM))
+      _sentence_type = _GPS_SENTENCE_GPGGA;
+    else if (!gpsstrcmp(_term, _GNGGA_TERM))
       _sentence_type = _GPS_SENTENCE_GPGGA;
     else
       _sentence_type = _GPS_SENTENCE_OTHER;
